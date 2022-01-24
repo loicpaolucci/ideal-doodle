e9c259d0ca23ca8ad9d59dd00f71c8569c66dc6d
loicpaolucci <97405774+loicpaolucci@users.noreply.github.com>
2022-01-24 19:17:22 +0100

    lib

---
+++ 02_calculator.rb
@@ -0,0 +1,26 @@
+def add (a , b)
+    return (a + b)
+  end
+
+
+  def subtract (a , b)
+    return (a - b)
+  end
+
+  def sum (array) 
+   return array.sum
+  end
+
+  def multiply (a , b)
+    return (a * b)
+  end
+
+  def power (a , b)
+    a ** b
+    return (a ** b)
+  end
+
+  def factorial(n)
+    n = num
+  return (1..n).inject(:*) || 1
+  end
